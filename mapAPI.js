18f4a649fbe32750ddefe06107c463a90c585114
Allison Bessett <allison_17@hotmail.com>
2017-05-11 17:46:53 -0700

    Map shows

--- mapAPI.js
+++ mapAPI.js
@@ -3,54 +3,34 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 
+
+
+
 $(document).ready(function () {
-  var google;
+  var latitude = 65.5848;
+  var longitude = 22.1567; 
+  var zm = 4;
+  
   function initMap() {
-     console.log("working");
-    var lulea = {lat: 65.5848,  lng: 22.1567};
+    var lulea = {lat: latitude,  lng: longitude};
     var map = new google.maps.Map(document.getElementById('google-map'), {
-      zoom: 4,
+      zoom: zm,
       center: lulea
     });
     var marker = new google.maps.Marker({
       position: lulea,
       map: map
     });
-  }
+}
   initMap();
+  $('#tSubmit').click(function (event) { //submit  
+    console.log("is submit working?");  
+    var latitude = $('#latitude').val();
+    var longitude = $('#longitude').val(); 
+    var zm = $('#zoom').val();
+    console.log(latitude + " " + longitude);
+    initMap();
+    event.preventDefault; 
   });
-//    var mapOptions = { 
-//      center: new google.maps.LatLng(65.5848, 22.1567),
-//      mapTypeId: google.maps.mapTypeId.ROADMAP, 
-//      zoom: 10
-//    };
-//    var map;
-//    var googleMap = new google.maps.Map(document.getElementById('map'), mapOptions);
-//  }
-//  
-//  function loadScript() {
-//    var script = document.createElement('script');
-//    script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&ccallback=init';
-//    document.body.appendChild(script);
-//  }
-//  
-//  window.onload = loadScript;
+});
 
-    
-  
-//    var googleMap = new google.maps.Map($("#google-map")[0]);
-//    googleMap.setCenter({lat: 65.5848, lng: 22.1567});
-//    googleMap.setZoom(10);
-//
-//    var marker = new google.maps.Marker({
-//      position: {lat: 65.5848, lng: 22.1567}
-//    });
-//    marker.setMap(googleMap);
-//
-//    var information = new google.maps.information({
-//      content: "Lulea"
-//    });
-//    marker.addListener("click", function () {
-//      information.open(googleMap, marker);
-//    });
-//  });
